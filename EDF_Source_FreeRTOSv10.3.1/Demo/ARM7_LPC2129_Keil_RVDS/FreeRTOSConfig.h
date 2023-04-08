7a503a4c0d6d26348323a5338abdf35a6b265e96
Msamy <msamygawad@gmail.com>
2023-04-08 15:25:51 +0200

    Project Done with video illustrations

--- EDF_Source_FreeRTOSv10.3.1/Demo/ARM7_LPC2129_Keil_RVDS/FreeRTOSConfig.h
+++ EDF_Source_FreeRTOSv10.3.1/Demo/ARM7_LPC2129_Keil_RVDS/FreeRTOSConfig.h
@@ -70,7 +70,7 @@ to exclude the API function. */
 #define INCLUDE_vTaskCleanUpResources	0
 #define INCLUDE_vTaskSuspend			1
 #define INCLUDE_vTaskDelayUntil			1
-#define INCLUDE_vTaskDelay					1
+#define INCLUDE_vTaskDelay				1
 
 /*######################################## Samy_EDF_Edits_Start ########################################*/
 #include "GPIO.h"
